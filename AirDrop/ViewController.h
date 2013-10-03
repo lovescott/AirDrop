ac8270c3ae447db0c2db0cfa97fe717f77cbac1a
Scott E Love <scott.love@gmail.com>
2013-10-03 07:25:13 -0400

    First Commit

---
+++ AirDrop/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AirDrop
+//
+//  Created by Scott E Love on 10/3/13.
+//  Copyright (c) 2013 Scott E Love. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ AirDrop/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AirDrop
+//
+//  Created by Scott E Love on 10/3/13.
+//  Copyright (c) 2013 Scott E Love. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
